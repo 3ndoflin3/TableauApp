a33d2c3c7d8aa14f8fbe721f86b82242cab45590
3ndoflin3 <kirylbilbao@hotmail.com>
2020-03-04 08:35:48 +0100

    check native platform test

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { cards } from '../app-routing.module';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -11,9 +12,27 @@ export class Tab1Page {
 
   cards: Array<any>;
   selectedCard: any;
-  constructor(private iab: InAppBrowser) {
+  constructor(private iab: InAppBrowser, public platform: Platform) {
     this.cards = cards;
     console.debug('TabsPage constructor %o', this.cards);
+    console.debug('Platform %o', platform.platforms);
+    if(platform.is('desktop')){
+      console.debug('Es desktop');
+    }
+    if(platform.is('ios')){
+      console.debug('Es ios');
+    }
+    if(platform.is('cordova')){
+      console.debug('Es cordova');
+    }
+    if(platform.is('mobile')){
+      console.debug('Es movil');
+    }
+    if(platform.is('android')){
+      console.debug('Es android');
+    }
+
+    
   }
 
   seleccionarElemento(c){
