5c335450648c03f662577c59486970360bb621be
3ndoflin3 <kirylbilbao@hotmail.com>
2020-03-05 08:51:07 +0100

    optimized code & single tab app

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { cards } from '../app-routing.module';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { Platform } from '@ionic/angular';
+import {Plugins} from '@capacitor/core';
+const { Browser } = Plugins;
 
 
 @Component({
@@ -9,89 +11,28 @@ import { Platform } from '@ionic/angular';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit{ 
+export class Tab1Page{ 
 
-  ngOnInit(): void {
-    
-  }
 
   cards: Array<any>;
   selectedCard: any;
   constructor(private iab: InAppBrowser, public platform: Platform) {
-    this.cards = cards;
-    console.debug('TabsPage constructor %o', this.cards);
-    console.debug('Platform %o', platform.platforms);    
-
-    if(this.platform.is('desktop')){
-      console.debug('Es desktop');
-    }
-    if(this.platform.is('ios')){
-      console.debug('Es ios');
-    }
-    if(this.platform.is('cordova')){
-      console.debug('Es cordova');
-    }
-    if(this.platform.is('mobile')){
-      console.debug('Es movil');
-    }
-    if(this.platform.is('android')){
-      console.debug('Es android');
-    }
+    this.cards = cards;  
   }
 
   seleccionarElemento(c){
     this.selectedCard = c;
   }
 
-  openBlank(c){
-    this.seleccionarElemento(c);
-    
-    this.platform.ready().then(() => {
-      if(this.platform.is('desktop')){
-        console.debug('Es desktop');
-        /* let browser = new InAppBrowser('https://ionic.io', '_system');
-        browser.on("loadstop").subscribe(()=> console.log("loadstop"));
-        this.iab.create(this.selectedCard.webViewUri, `_blank`, {location:'no'});
- */
-      }
-      if(this.platform.is('ios')){
-        console.debug('Es ios');
-        this.iab.create(this.selectedCard.webViewUri, `_blank`, {location:'no'});
-
-      }
-      if(this.platform.is('cordova')){
-        console.debug('Es cordova');
-      }
-      if(this.platform.is('mobile')){
-        console.debug('Es movil');
-      }
-      if(this.platform.is('android')){
-        console.debug('Es android');
-        this.iab.create(this.selectedCard.webViewUri, `_blank`, {location:'no'});
-
-      }
-    });
-  }
-
   openSystem(c){
     this.seleccionarElemento(c);
     this.platform.ready().then(() => {
-      if(this.platform.is('desktop')){
-        console.debug('Es desktop');
-        this.iab.create(this.selectedCard.webViewUri, `_self`, {location:'no'});
 
-      }
       if(this.platform.is('ios')){
         console.debug('Es ios');
-        this.iab.create(this.selectedCard.webViewUri, `_self`, {toolbar:'yes'});
+        Browser.open({toolbarColor: "#f4dc41", url: this.cards[0].webViewUri});
+      }
 
-      }
-      if(this.platform.is('cordova')){
-        console.debug('Es cordova');
-      }
-      if(this.platform.is('mobile')){
-        console.debug('Es movil');
-      }
       if(this.platform.is('android')){
         console.debug('Es android');
         this.iab.create(this.selectedCard.webViewUri, `_self`, {toolbar:'yes'});
