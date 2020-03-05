b137fb6e1f4dcb422f79c86937f5adde9d7f53a0
3ndoflin3 <kirylbilbao@hotmail.com>
2020-03-05 13:26:15 +0100

    every card has a route to open

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,24 +25,21 @@ export const cards = [
       title: 'Urban Audit',
       imageUri: '../assets/urban_audit.png',
       description: 'Urban Audit city factsheets for 2019 Urban Adaptation Map Viewer.',
-      //webViewUri: 'https://developer.tableau.com',
-      //webViewUri: 'https://public.tableau.com/profile/european.environment.agency#!/vizhome/test_20190116Urban_vulnerability_ideasFR_0/mainpage',
       webViewUri: 'https://tableau.discomap.eea.europa.eu/views/UrbanAdaptationMapViewer_fc_phone_test/mainpage?iframeSizedToWindow=true&:embed=y&:showAppBanner=false&:display_count=no&:showVizHome=no&:origin=viz_share_link'
   }, {
       title: 'Option 2',
       imageUri: '../assets/StoriesViz.png',
       description: 'Option 2 - Description',
-      //webViewUri: 'https://onlinehelp.tableau.com/current/api/js_api/en-us/JavaScriptAPI/js_api.htm',
-      webViewUri: ''
+      webViewUri: 'https://tableau.discomap.eea.europa.eu/t/Natureonline/views/NS1_GeneralinformationonNatura2000/1_GeneralinformationontheNatura2000network?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link'
   }, {
       title: 'Option 3',
       imageUri: '../assets/StoriesViz.png',
       description: 'Option 3 - Description',
-      webViewUri: ''
+      webViewUri: 'https://tableau.discomap.eea.europa.eu/t/Natureonline/views/Art17_NS5_Pressuresthreats/5a_Mainpressuresthreats_habitats?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link'
   }, {
       title: 'Option 4',
       imageUri: '../assets/StoriesViz.png',
       description: 'Option 4 - Description',
-      webViewUri: ''
+      webViewUri: 'https://tableau.discomap.eea.europa.eu/t/Natureonline/views/Art17_NS6_Natura2000coverage/6aNatura2000coverageforhabitats?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link'
   }
 ];
